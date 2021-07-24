7ad6838504d36a4f9c885e5062356695a4c98000
Faith Omotoso <faithomotoso@gmail.com>
2021-07-24 13:36:01 +0100

    Added loading indicator for webview

--- lib/src/api/PaystackApi.dart
+++ lib/src/api/PaystackApi.dart
@@ -9,7 +9,7 @@ class PaystackApi {
   /// Stores the [callbackUrl]
   static String _callbackUrl;
 
-  /// Default [callbackUrl] if null
+  /// Default [callbackUrl] if null, shouldn't be null
   static String _defaultCallbackUrl = "https://www.paystack.com";
 
   /// Returns a [callbackUrl]
@@ -19,57 +19,44 @@ class PaystackApi {
     return _callbackUrl ?? _defaultCallbackUrl;
   }
 
-  /// Initialize [PaystackApi] with a callbackURL and [secretKey]
-  static init({@required String callbackUrl, @required String secretKey}) {
-    _appendSecretKey(secretKey);
+  /// Initialize [PaystackApi] with a callbackURL
+  static init({@required String callbackUrl}) {
     PaystackApi._callbackUrl = callbackUrl;
-  }
-
-  static void _assertSecretKey() {
-    assert(_dio.options.headers.containsKey("Authorization"),
-        "Paystack secret key not found");
-  }
-
-  /// Append [secretKey] to dio header
-  static void _appendSecretKey(String secretKey) {
-    _dio.options.headers["Authorization"] = "Bearer $secretKey";
   }
 
   /// Initializes a transaction
   ///
   /// Makes an api call with the customers email and amount
   /// Gets the [PaystackInitialize.authUrl] to be used in a [WebView]
-  static Future initializeTransaction(
-      {@required String customerEmail, @required double amount, Map<String, dynamic> extraData}) async {
-    _assertSecretKey();
-
-    // using raw dio.post for error to be handled in the future builder
-    try {
-      Response response = await _dio.post("/transaction/initialize", data: {
-        "email": customerEmail,
-        "amount": (amount * 100).toString(),
-        "callback_url": callbackUrl,
-        ...extraData
-      });
-      return response;
-    } on DioError catch (e) {
-      debugPrint("$_packageName: Error initializing transaction: ${e.response}");
-      if (e.response.data.containsKey("message")) {
-        if (e.response.data["message"]
-            .toString()
-            .toLowerCase()
-            .contains("key")) {
-         print("$_packageName: Please check your Paystack secret key");
-        }
-      }
-      throw e;
-    }
-  }
+  // static Future initializeTransaction(
+  //     {@required String customerEmail, @required double amount, Map<String, dynamic> extraData}) async {
+  //
+  //   // using raw dio.post for error to be handled in the future builder
+  //   try {
+  //     Response response = await _dio.post("/transaction/initialize", data: {
+  //       "email": customerEmail,
+  //       "amount": (amount * 100).toString(),
+  //       "callback_url": callbackUrl,
+  //       ...extraData
+  //     });
+  //     return response;
+  //   } on DioError catch (e) {
+  //     debugPrint("$_packageName: Error initializing transaction: ${e.response}");
+  //     if (e.response.data.containsKey("message")) {
+  //       if (e.response.data["message"]
+  //           .toString()
+  //           .toLowerCase()
+  //           .contains("key")) {
+  //        print("$_packageName: Please check your Paystack secret key");
+  //       }
+  //     }
+  //     throw e;
+  //   }
+  // }
 
   /// Verifies a transaction
   static Future verifyTransaction(
       {@required String transactionReference}) async {
-    _assertSecretKey();
     assert(
         transactionReference != null, "Transaction reference must not be null");
 
--- lib/src/paystack_webview.dart
+++ lib/src/paystack_webview.dart
@@ -5,7 +5,6 @@ import 'package:flutter_paystack_webview/src/components/error_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/platform_interface.dart';
 import 'api/PaystackApi.dart';
-import 'components/pkg_future_builder.dart';
 import 'models/PaystackInitialize.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -16,26 +15,18 @@ import 'models/WebviewError.dart';
 /// The [authUrl] gotten from initialization is loaded in a [WebView].
 
 class PaystackWebView extends StatefulWidget {
-  /// Secret key gotten from Paystack
-  final String secretKey;
+  /// URL gotten from paystack after initializing.
+  /// Initialization should either be from your backend or done locally
+  final String paymentURL;
 
-  /// Email of the customer you're charging.
-  final String customerEmail;
-
-  /// Amount to charge, in Naira.
-  ///
-  /// Would be converted to Kobo automatically.
-  final double amountInNaira;
-
-  /// Extra data to pass to the initialize api
-  /// https://paystack.com/docs/api/#transaction
-  final Map<String, dynamic> extraInitData;
+  /// Called when a transaction has been completed and webview closed (fullscreen)
+  final VoidCallback onTransactionCompleted;
 
   /// Callback after a transaction has been initialized.
   ///
   /// Returns a [PaystackInitialize] object.
   /// https://paystack.com/docs/api/#transaction-initialize
-  final OnTransactionInitialize onTransactionInitialized;
+  // final OnTransactionInitialize onTransactionInitialized;
 
   /// Callback after a transaction has been verified.
   ///
@@ -44,7 +35,7 @@ class PaystackWebView extends StatefulWidget {
   /// if a transaction was successful or not.
   /// Check the ["status"] for the status of the transaction
   /// https://paystack.com/docs/api/#transaction-verify
-  final OnTransactionVerified onTransactionVerified;
+  // final OnTransactionVerified onTransactionVerified;
 
   /// Callback URL appended to the initialization request body.
   /// Must start with https:// or http://
@@ -71,25 +62,15 @@ class PaystackWebView extends StatefulWidget {
   ///   )
   /// ```
   /// This removes the back button and prevents popping the widget when the transaction is complete
-  /// Check example folder...
+  /// Check the example folder...
   final bool usingEmbedded;
 
   PaystackWebView(
-      {@required this.secretKey,
-      @required this.customerEmail,
-      @required this.amountInNaira,
-      @required this.onTransactionVerified,
+      {@required this.paymentURL,
       @required this.callbackURL,
-      this.extraInitData,
-      this.usingEmbedded = false,
-      this.onTransactionInitialized})
-      : assert(secretKey != null, "Paystack secret key must not be null"),
-        assert(customerEmail != null, "Customer email must not be null"),
-        assert(amountInNaira != null, "Amount must not be null"),
-        assert(amountInNaira > 0, "Amount can not be negative"),
-        assert(onTransactionVerified != null,
-            "onTransactionVerified must not be null"),
-        assert(
+      @required this.onTransactionCompleted,
+      this.usingEmbedded = false})
+      : assert(
             callbackURL.contains("https://") || callbackURL.contains("http://"),
             "Callback URL must begin with 'https:// or http://");
 
@@ -98,35 +79,24 @@ class PaystackWebView extends StatefulWidget {
 }
 
 class _PaystackWebViewState extends State<PaystackWebView> {
-  Future initializingFuture;
-  Future verifyingFuture;
-
-  // When true, shows the webView
-  // When false, shows the verifyingTransactionIndicator
-  ValueNotifier<bool> showWebView = ValueNotifier<bool>(false);
-  ValueNotifier<bool> showVerification = ValueNotifier<bool>(false);
-  ValueNotifier<bool> webViewError = ValueNotifier<bool>(false);
+  final ValueNotifier<bool> webViewError = ValueNotifier<bool>(false);
+  final ValueNotifier<bool> loadingWebview = ValueNotifier<bool>(false);
   WebViewController webViewController;
 
   PaystackInitialize paystackInitialize;
 
   // Set to true when widget.usingEmbedded is true and transaction
-  // has been verified
+  // has been completed
   bool showBlank = false;
 
   @override
   void initState() {
     super.initState();
-    PaystackApi.init(
-        callbackUrl: widget.callbackURL, secretKey: widget.secretKey);
-    initializeTransaction();
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
   void dispose() {
-    showWebView.dispose();
-    showVerification.dispose();
     webViewError.dispose();
 
     super.dispose();
@@ -144,93 +114,50 @@ class _PaystackWebViewState extends State<PaystackWebView> {
                 children: [
                   if (!widget.usingEmbedded) BackButton(),
                   Expanded(
-                    child: PackageFutureBuilder(
-                      future: initializingFuture,
-                      onRefresh: () async {
-                        setState(() {
-                          initializeTransaction();
-                        });
-                      },
-                      loadingWidget: _defaultInitializingIndicator(),
-                      child: ValueListenableBuilder(
-                        valueListenable: showWebView,
-                        builder: (context, showWebView, child) {
-                          // if (!showWebView) return _defaultVerifyingIndicator();
-                          if (!showWebView) return verifyingWidget();
+                    child: ValueListenableBuilder(
+                        valueListenable: webViewError,
+                        builder: (context, wError, child) {
+                          if (wError)
+                            return PkgErrorWidget(
+                                errorMessage:
+                                    "An error occurred while loading. Tap to reload.",
+                                onRefresh: () {
+                                  webViewError.value = false;
+                                  // webViewController.reload();
+                                });
 
                           return ValueListenableBuilder(
-                              valueListenable: webViewError,
-                              builder: (context, webViewHasError, child) {
-                                if (webViewHasError)
-                                  return PkgErrorWidget(
-                                      errorMessage:
-                                          "An error occurred while loading. Tap to reload.",
-                                      onRefresh: () {
-                                        webViewError.value = false;
-                                        // webViewController.reload();
-                                      });
-
-                                return WebView(
-                                  initialUrl: paystackInitialize?.authUrl ?? "",
-                                  onWebViewCreated: (controller) {
-                                    webViewController = controller;
-                                  },
-                                  javascriptMode: JavascriptMode.unrestricted,
-                                  navigationDelegate: navigationDelegate,
-                                  onWebResourceError: handleWebResourceError,
+                              valueListenable: loadingWebview,
+                              builder: (context, loading, child) {
+                                return IndexedStack(
+                                  index: loading ? 0 : 1,
+                                  children: [
+                                    _colLoadingIndicator(
+                                        "Loading payment gateway..."),
+                                    WebView(
+                                      initialUrl: widget.paymentURL,
+                                      onWebViewCreated: (controller) {
+                                        webViewController = controller;
+                                        loadingWebview.value = true;
+                                      },
+                                      onPageFinished: (_) {
+                                        loadingWebview.value = false;
+                                      },
+                                      javascriptMode:
+                                          JavascriptMode.unrestricted,
+                                      navigationDelegate: navigationDelegate,
+                                      onWebResourceError:
+                                          handleWebResourceError,
+                                    )
+                                  ],
                                 );
                               });
-                        },
-                      ),
-                    ),
+                        }),
                   )
                 ],
               ),
       ),
     );
-  }
-
-  Future initializeTransaction() async {
-    initializingFuture = PaystackApi.initializeTransaction(
-            customerEmail: widget.customerEmail,
-            amount: widget.amountInNaira,
-            extraData: widget.extraInitData ?? {})
-        .then((value) {
-      // paystackInitialize = PaystackInitialize.fromJson(value.data["data"]);
-      Map<String, dynamic> data = value.data["data"];
-      paystackInitialize = PaystackInitialize(
-          authUrl: data["authorization_url"],
-          reference: data["reference"],
-          accessCode: data["access_code"]);
-      widget.onTransactionInitialized?.call(paystackInitialize);
-      showWebView.value = true;
-      return value;
-    });
-  }
-
-  Future verifyTransaction() async {
-    showWebView.value = false;
-    showVerification.value = true;
-    verifyingFuture = PaystackApi.verifyTransaction(
-            transactionReference: paystackInitialize?.reference)
-        .then((value) {
-      showVerification.value = false;
-      // Close web-view
-      // Only close the WebView when the widget is being used as a full screen
-      // (by Navigation)
-      if (!widget.usingEmbedded)
-        Navigator.pop(context);
-      else
-        setState(() {
-          showBlank = true;
-        });
-
-      Map<String, dynamic> data = value.data["data"];
-      widget.onTransactionVerified
-          ?.call(data, data["status"], data["reference"]);
-
-      return value;
-    });
   }
 
   Future<NavigationDecision> navigationDelegate(
@@ -239,9 +166,17 @@ class _PaystackWebViewState extends State<PaystackWebView> {
     if (request.url.contains("tel")) {
       // Handling event when a user taps on a USSD code
       if (await canLaunch(request.url)) launch(request.url);
-    } else if (request.url.contains(PaystackApi.callbackUrl)) {
-      // Transaction complete, verifyTransaction
-      verifyTransaction();
+    } else if (request.url.contains(widget.callbackURL)) {
+      // Transaction complete
+
+      if (!widget.usingEmbedded) {
+        Navigator.pop(context);
+      } else {
+        setState(() {
+          showBlank = true;
+        });
+      }
+      widget.onTransactionCompleted?.call();
     } else if (request.url.contains("checkout")) {
       // Fixes blank screen on iOS
       return NavigationDecision.navigate;
@@ -271,33 +206,6 @@ class _PaystackWebViewState extends State<PaystackWebView> {
     );
   }
 
-  Widget _defaultInitializingIndicator() {
-    return _colLoadingIndicator("Initializing Transaction");
-  }
-
-  Widget _defaultVerifyingIndicator() {
-    return _colLoadingIndicator("Verifying Transaction");
-  }
-
-  Widget verifyingWidget() {
-    return ValueListenableBuilder(
-      valueListenable: showVerification,
-      builder: (context, showVerification, child) {
-        if (!showVerification) return SizedBox();
-
-        return PackageFutureBuilder(
-            future: verifyingFuture,
-            onRefresh: () async {
-              setState(() {
-                verifyTransaction();
-              });
-            },
-            loadingWidget: _defaultVerifyingIndicator(),
-            child: Container());
-      },
-    );
-  }
-
   void handleWebResourceError(WebResourceError webResourceError) {
     String errorDescription = webResourceError.description;
     if (WebViewError.hasWebviewError(errorDescription)) {
@@ -311,4 +219,3 @@ typedef OnTransactionInitialize(PaystackInitialize paystackInitialize);
 
 typedef OnTransactionVerified(
     Map verificationMap, String status, String reference);
-
